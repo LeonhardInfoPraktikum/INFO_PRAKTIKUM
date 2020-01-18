edaf190955bf5636a929955498bf9b7f6e6ef045
LeonhardInfoPraktikum <leonhard.janzen@web.de>
2020-01-18 12:21:19 +0100

    Variante B hinzugefügt

--- myClient.c
+++ myClient.c
@@ -22,6 +22,7 @@ using namespace std;
 
 
 int varianteA(TCPclient *ptrC);
+int varianteB(TCPclient *ptrC);
 
 
 int main()
@@ -33,16 +34,57 @@ int main()
 
 	c.conn(host , 2024);
 
+	cout << "Durchlauf aller Felder:" << endl;
 
 	for(int i=0; i < 20; i++){
 		nmbShoots = varianteA(&c);
+		cout << nmbShoots << endl;
+	}
+	cout << "Zufällige Koordinaten:" << endl;
+	for(int i=0; i < 20; i++){
+		nmbShoots = varianteB(&c);
 		cout << nmbShoots << endl;
 	}
 
 }
 
 
+int varianteB(TCPclient *ptrC)
+{
+	stringstream *strStream = NULL;
+	string msg;
+	int counter = 0;
+	int r;
+	int x;
 
+	ptrC->sendData(string("NEWGAME"));
+	msg = ptrC->receive(32);
+
+
+	while (msg.compare(0,5,"RES[4") != 0)
+	{
+
+
+
+		if(strStream != NULL)
+		{
+			delete strStream;
+		}
+
+		int x = (rand() % 10)+1;
+
+		int y = (rand() % 10)+1;
+
+		strStream = new stringstream();
+		*strStream << "COORD["<<x<<","<<y<<"]";
+		ptrC->sendData(strStream->str());
+		msg = ptrC -> receive(32);
+		counter++;
+	}
+
+
+return counter;
+}
 
 int varianteA(TCPclient *ptrC)
 {
@@ -54,16 +96,20 @@ int varianteA(TCPclient *ptrC)
 	int r,s;
 
 
-	ptrC->sendData(string("NEWGAME"));
-	msg = ptrC->receive(32);
+	ptrC->sendData(string("NEWGAME")); //startet neues spiel
+	msg = ptrC->receive(32);			//erwartet antwoRT
 
 	while (y <= 10)
 	{
+
 		while (x <= 10)
 		{
-			if(strStream != NULL){
+
+			if(strStream != NULL)
+			{
 				delete strStream;
 			}
+
 			strStream = new stringstream();
 			*strStream << "COORD["<<x<<","<<y<<"]";
 			ptrC->sendData(strStream->str());
@@ -75,13 +121,22 @@ int varianteA(TCPclient *ptrC)
 			{
 				counter++;
 			}
+
 			else
 			{
 				cout << "unbekannt:" << msg << endl;
 			}
-			if (r == TASK3::GAME_OVER) {break;};
+
+			if (r == TASK3::GAME_OVER)
+			{
+				break;
+			};
+
 		}
-		if (r == TASK3::GAME_OVER){ break;};
+		if (r == TASK3::GAME_OVER)
+		{
+			break;
+		};
 
 		y++;
 		x = 1;
